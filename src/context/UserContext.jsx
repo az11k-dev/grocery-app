4da2460fbe79ca66ff65ad20d0d3328e92203973
az11k-dev <bazaclash55@gmail.com>
2025-06-28 21:31:33 +0500

    profile-test

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -12,24 +12,38 @@ export const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const checkAuth = async () => {
-            const { data, error } = await supabase.auth.getUser()
+    const checkAuth = async () => {
+        const { data, error } = await supabase.auth.getUser()
 
-            if (error || !data.user) {
-                console.error(error);
-            } else {
-                const { data: profile } = await supabase
-                    .from('profiles')
-                    .select('*')
-                    .eq('id', data.user.id)
-                    .single()
-                setUser(profile);
-            }
-
-            setLoading(false);
+        if (error || !data.user) {
+            console.error(error);
+        } else {
+            const { data: profile } = await supabase
+                .from('profiles')
+                .select('*')
+                .eq('id', data.user.id)
+                .single()
+            setUser(profile);
         }
-        checkAuth()
+
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        checkAuth();
+    }, []);
+
+    useEffect(() => {
+        const {data: listener} = supabase.auth.onAuthStateChange((event, session) => {
+            if (session) {
+                checkAuth();
+            } else {
+                setUser(null);
+            }
+        });
+        return () => {
+            listener.subscription.unsubscribe();
+        };
     }, []);
 
     return (
